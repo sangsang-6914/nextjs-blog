c6e506d2e4d7b995c422690e5ce00ae6ca7084d0
sangsang-6914 <sangsang6914@gmail.com>
2023-05-07 22:48:53 +0900

    불필요한 코드 제거

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -1,14 +1,13 @@
-import { getAllPosts, getFeaturedPosts } from '@/api/posts';
+import { getFeaturedPosts } from '@/api/posts';
 import React from 'react';
 import GridPosts from './GridPosts';
-import PostCard from './PostCard';
 
 async function FeaturedPosts() {
   const posts = await getFeaturedPosts();
 
   return (
     <section className="mt-3">
-      <h1 className="text-xl font-bold">Featured Posts</h1>
+      <h1 className="text-2xl font-bold">Featured Posts</h1>
       <GridPosts posts={posts} />
     </section>
   );
