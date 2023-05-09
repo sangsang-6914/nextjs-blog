b90df2db978df2d23ec8cb3bc99b5604510a6b9b
sangsang-6914 <sangsang6914@gmail.com>
2023-05-09 18:39:27 +0900

    마크다운 뷰어 추가, nextjs image 사용 설정 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import { getPost } from '@/api/posts';
-import { readFileSync } from 'fs';
-import path from 'path';
+import MarkdownViewer from '@/components/MarkdownViewer';
+import Image from 'next/image';
 import React from 'react';
+import { AiTwotoneCalendar } from 'react-icons/ai';
 
 interface Props {
   params: {
@@ -11,8 +12,28 @@ interface Props {
 
 async function PostDetailPage({ params: { slug } }: Props) {
   const post = await getPost(slug);
+  const { title, description, content, path, date } = post;
 
-  return <pre>{post.content}</pre>;
+  return (
+    <article className="bg-gray-200 rounded-2xl overflow-hidden m-4">
+      <Image
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={500}
+        height={300}
+        className="w-full max-h-96"
+      />
+      <div className="p-5 flex flex-col">
+        <time className="self-end text-sky-400 font-bold flex items-center gap-2">
+          <AiTwotoneCalendar size={20} /> {date}
+        </time>
+        <h1 className="text-3xl font-bold">{title}</h1>
+        <p className="font-bold">{description}</p>
+        <div className="border-2 border-sky-500 w-48 mt-2 mb-5" />
+        <MarkdownViewer content={content} />
+      </div>
+    </article>
+  );
 }
 
 export default PostDetailPage;
